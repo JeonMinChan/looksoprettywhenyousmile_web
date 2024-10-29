d90cfd0cdfa79903b532fc2fcea2b9ef21b3bb5a
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-10-29 21:14:17 +0900

    feat :: BackGroundComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BackGround from "./components/common/BackGround";
+import BackGRoundImg from "@src/assets/img/defaultBackground.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          B1ND AUTH-TEMPLATE
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Team-B1ND/B1ND-AUTH-TEMPLATE"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          B1NDGithub
-        </a>
-      </header>
+      <BackGround backgroundImgUrl={BackGRoundImg}>asdf</BackGround>
     </div>
   );
 }
