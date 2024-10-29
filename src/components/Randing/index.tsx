294002a1acb20392ba7613ada3809bf486fc7f7b
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-10-29 21:31:34 +0900

    feat :: frameInput navigate

--- src/components/Randing/index.tsx
+++ src/components/Randing/index.tsx
@@ -3,8 +3,10 @@ import * as S from "./style";
 import Bear1 from "@src/assets/images/bear1.png";
 import Bear2 from "@src/assets/images/bear2.png";
 import Bear3 from "@src/assets/images/bear3.png";
+import { useNavigate } from "react-router-dom";
 
 const Randing = () => {
+  const navigate = useNavigate();
   return (
     <S.Layout>
       <S.ImgContainer>
@@ -14,7 +16,7 @@ const Randing = () => {
       </S.ImgContainer>
       <S.Title>넌 웃을 때가 예쁘더라</S.Title>
       <S.SubTitle>세계 최고의, 스티커 사진 서비스.</S.SubTitle>
-      <S.StartBtn>시작하기</S.StartBtn>
+      <S.StartBtn onClick={() => navigate("/frame-input")}>시작하기</S.StartBtn>
     </S.Layout>
   );
 };
