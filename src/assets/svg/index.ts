524260c422819cc702887ad2fa1142f0ba2e6fee
gjaegyun <jaegyungim06@gmail.com>
2024-10-30 01:13:48 +0900

    feat :: icon 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,2 +1,4 @@
 export { default as ArrowIcon } from './ArrowIcon';
 export { default as PreviousIcon } from './PreviousIcon';
+export { default as CameraIcon } from './CameraIcon';
+export { default as SaveIcon } from './SaveIcon';
