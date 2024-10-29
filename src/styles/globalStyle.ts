911fbc2dc61e7ec666b27fefbdedd21b5ebe5643
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-10-29 20:22:16 +0900

    design:: globalStyle

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Pretendard-Medium";
+    src: url("/Pretendard-Medium.otf") format("opentype");
+  }
+  ${reset}
+  * {
+    margin: 0;
+    padding: 0;
+    border: none;
+    font-family: "Pretendard-Medium" !important;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
