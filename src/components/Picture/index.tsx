06accee092b4eefc8c54e761346d813e2f58bf97
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-10-30 01:51:22 +0900

    feat :: 사진 촬영 & 로컬 스토리지 저장

--- src/components/Picture/index.tsx
+++ src/components/Picture/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useRef, useState, useEffect } from "react";
 import BackGround from "../common/BackGround";
 import BackGRoundImg from "@src/assets/img/defaultBackground.svg";
 import GoBack from "@src/assets/img/goBack.svg";
@@ -6,18 +6,75 @@ import WhiteCamara from "@src/assets/img/whiteCamara.svg";
 import * as S from "./style";
 import Webcam from "react-webcam";
 
-const Picture: React.FC = () => {
+const Picture = () => {
+  const webcamRef = useRef<Webcam>(null);
+  const [isTakingPictures, setIsTakingPictures] = useState(false);
+  const [pictureCount, setPictureCount] = useState(0);
+  const [countdown, setCountdown] = useState<number | null>(null);
+  const [flash, setFlash] = useState(false);
+
+  const capturePhoto = useCallback(() => {
+    if (webcamRef.current) {
+      const imageSrc = webcamRef.current.getScreenshot();
+      if (imageSrc) {
+        localStorage.setItem(`photo_${pictureCount + 1}`, imageSrc);
+        setPictureCount((prevCount) => prevCount + 1);
+      }
+    }
+    setFlash(true);
+    setTimeout(() => setFlash(false), 500);
+  }, [pictureCount]);
+
+  const startTakingPictures = () => {
+    setIsTakingPictures(true);
+    setPictureCount(0);
+    setCountdown(5);
+  };
+
+  useEffect(() => {
+    let countdownTimer: NodeJS.Timeout;
+    if (isTakingPictures && pictureCount < 8) {
+      if (countdown === null) {
+        setCountdown(5);
+      } else if (countdown > 0) {
+        countdownTimer = setTimeout(
+          () => setCountdown((prev) => (prev as number) - 1),
+          1000,
+        );
+      } else if (countdown === 0) {
+        capturePhoto();
+        setCountdown(5);
+      }
+    } else if (pictureCount === 8) {
+      setIsTakingPictures(false);
+      setCountdown(null);
+    }
+
+    return () => clearTimeout(countdownTimer);
+  }, [isTakingPictures, pictureCount, countdown, capturePhoto]);
+
   return (
     <S.Layout>
       <BackGround backgroundImgUrl={BackGRoundImg}>
         <S.Container>
           <S.GoBack src={GoBack} alt="뒤로가기" />
-          <S.Camera>
-            <Webcam width={1280} height={880} mirrored={true} />
+          <S.Camera flash={flash}>
+            <Webcam
+              ref={webcamRef}
+              width={1280}
+              height={880}
+              mirrored={true}
+              screenshotFormat="image/png"
+            />
+            {countdown !== null && <S.Timer>{countdown}</S.Timer>}
           </S.Camera>
           <S.PictureDetailContainer>
-            <S.PictureNum>1/8</S.PictureNum>
-            <S.WhiteCamara src={WhiteCamara} alt="하얀색카메라" />
+            <S.PictureNum>{pictureCount}/8</S.PictureNum>
+            <S.WhiteCamara
+              src={WhiteCamara}
+              alt="하얀색카메라"
+              onClick={startTakingPictures}
+            />
           </S.PictureDetailContainer>
         </S.Container>
       </BackGround>
