102403efa1e6080d7b18a4e620e3b135e2ec4d18
gjaegyun <jaegyungim06@gmail.com>
2024-10-30 13:48:04 +0900

    feat :: app 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import GlobalStyle from "./styles/globalStyle";
-import { FrameInputPage, Randing, SideBar, ChooseFrame } from "./components";
+import React, { useRef, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './styles/globalStyle';
+import { FrameInputPage, Randing, SideBar, ChooseFrame } from './components';
 
-import BackGround from "./components/common/BackGround";
-import BackGRoundImg from "@src/assets/img/defaultBackground.svg";
-import ImageInput from "./components/ImageInputPage";
-import Picture from "./components/Picture";
+import BackGround from './components/common/BackGround';
+import BackGRoundImg from '@src/assets/img/defaultBackground.svg';
+import ImageInput from './components/ImageInputPage';
+import Picture from './components/Picture';
 
 function App() {
   const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false);
-  const [imgUrl, setImgUrl] = useState<string>("");
+  const [imgUrl, setImgUrl] = useState<string>('');
+  const sideBarRef = useRef<HTMLDivElement | null>(null);
 
   return (
     <BrowserRouter>
@@ -23,11 +24,14 @@ function App() {
             path="/frame-input"
             element={<FrameInputPage setIsSideBarOpen={setIsSideBarOpen} setImgUrl={setImgUrl} />}
           />
-          <Route path="photo" element={<Picture />} />
-          <Route path="image-input" element={<ImageInput />} />
+          <Route path="/photo" element={<Picture />} />
+          <Route
+            path="/image-input"
+            element={<ImageInput setIsSideBarOpen={setIsSideBarOpen} sideBarRef={sideBarRef} />}
+          />
         </Routes>
 
-        {isSideBarOpen && <SideBar imgUrl={imgUrl} />}
+        {isSideBarOpen && <SideBar ref={sideBarRef} imgUrl={imgUrl} setIsSideBarOpen={setIsSideBarOpen} />}
       </BackGround>
     </BrowserRouter>
   );
