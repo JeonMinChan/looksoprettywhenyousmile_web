91bfae64d3f943b41005880ced9f6469a8d6f255
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-10-30 14:25:50 +0900

    feat :: 라우터설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import GlobalStyle from './styles/globalStyle';
-import { FrameInputPage, Randing, SideBar, ChooseFrame, ShareToast } from './components';
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyle";
+import {
+  FrameInputPage,
+  SideBar,
+  ChooseFrame,
+  ShareToast,
+  Randing,
+} from "./components";
 
-import BackGround from './components/common/BackGround';
-import BackGRoundImg from '@src/assets/img/defaultBackground.svg';
+import BackGround from "./components/common/BackGround";
+import BackGRoundImg from "@src/assets/img/defaultBackground.svg";
+import FindFrame from "./components/FindFrame";
 
 function App() {
   const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false);
-  const [imgUrl, setImgUrl] = useState<string>('');
+  const [imgUrl, setImgUrl] = useState<string>("");
   const [isShowModal, setIsShowModal] = useState<boolean>(false);
 
   return (
@@ -17,7 +24,8 @@ function App() {
       <BackGround backgroundImgUrl={BackGRoundImg}>
         <Routes>
           <Route path="/" element={<Randing />} />
-          <Route path="/choose" element={<ChooseFrame />} />
+          <Route path="/frame-choose" element={<ChooseFrame />} />
+          <Route path="/frame-find" element={<FindFrame />} />
           <Route
             path="/frame-input"
             element={
@@ -28,7 +36,9 @@ function App() {
             }
           />
         </Routes>
-        {isSideBarOpen && <SideBar imgUrl={imgUrl} setIsShowModal={setIsShowModal} />}
+        {isSideBarOpen && (
+          <SideBar imgUrl={imgUrl} setIsShowModal={setIsShowModal} />
+        )}
         {isShowModal && <ShareToast setIsShowModal={setIsShowModal} />}
       </BackGround>
     </BrowserRouter>
