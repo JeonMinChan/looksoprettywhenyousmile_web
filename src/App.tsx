9fc6941414805f1b730b73cdedf1642a8f4671e4
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-10-30 22:52:37 +0900

    fix :: Randing -> Landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React, { useRef, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import GlobalStyle from './styles/globalStyle';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { FrameInputPage, SideBar, ChooseFrame, ShareToast, Randing } from './components';
+import React, { useRef, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyle";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { FrameInputPage, SideBar, ChooseFrame, ShareToast } from "./components";
 
-import BackGround from './components/common/BackGround';
-import BackGRoundImg from '@src/assets/img/defaultBackground.svg';
-import ImageInput from './components/ImageInputPage';
-import Picture from './components/Picture';
+import BackGround from "./components/common/BackGround";
+import BackGRoundImg from "@src/assets/img/defaultBackground.svg";
+import ImageInput from "./components/ImageInputPage";
+import Picture from "./components/Picture";
 
-import FindFrame from './components/FindFrame';
+import FindFrame from "./components/FindFrame";
+import Landing from "./components/Landing";
 
 function App() {
   const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false);
-  const [imgUrl, setImgUrl] = useState<string>('');
+  const [imgUrl, setImgUrl] = useState<string>("");
   const sideBarRef = useRef<HTMLDivElement | null>(null);
 
   const [isShowModal, setIsShowModal] = useState<boolean>(false);
@@ -34,17 +35,27 @@ function App() {
         <GlobalStyle />
         <BackGround backgroundImgUrl={BackGRoundImg}>
           <Routes>
-            <Route path="/" element={<Randing />} />
+            <Route path="/" element={<Landing />} />
             <Route path="/frame-choose" element={<ChooseFrame />} />
             <Route path="/frame-find" element={<FindFrame />} />
             <Route
               path="/frame-input"
-              element={<FrameInputPage setIsSideBarOpen={setIsSideBarOpen} setImgUrl={setImgUrl} />}
+              element={
+                <FrameInputPage
+                  setIsSideBarOpen={setIsSideBarOpen}
+                  setImgUrl={setImgUrl}
+                />
+              }
             />
             <Route path="/photo" element={<Picture />} />
             <Route
               path="/image-input"
-              element={<ImageInput setIsSideBarOpen={setIsSideBarOpen} sideBarRef={sideBarRef} />}
+              element={
+                <ImageInput
+                  setIsSideBarOpen={setIsSideBarOpen}
+                  sideBarRef={sideBarRef}
+                />
+              }
             />
           </Routes>
           {isSideBarOpen && (
