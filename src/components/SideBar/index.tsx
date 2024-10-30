9b7abab98d1641a395933dcdc2403a6673c62121
gjaegyun <jaegyungim06@gmail.com>
2024-10-30 22:16:42 +0900

    feat :: side bar conflict 해결

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, forwardRef } from "react";
+import React, { useEffect, useState, forwardRef } from 'react';
 import html2canvas from 'html2canvas';
 import * as S from './style';
 import { CameraIcon, DownLoadIcon, EditIcon, ShareIcon } from '@src/assets/svg';
@@ -14,13 +14,6 @@ interface SideBarProps {
   setIsShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const SideBar = forwardRef<HTMLDivElement, SideBarProps>(
-  ({ imgUrl, setIsSideBarOpen, setIsShowModal }, ref) => {
-    const navigate = useNavigate();
-
-    const { pathname } = useLocation();
-    const [path, setPath] = useState<string>("");
-=======
 const SideBar = forwardRef<HTMLDivElement, SideBarProps>(({ setIsSideBarOpen, setIsShowModal }, ref) => {
   const navigate = useNavigate();
 
@@ -29,45 +22,40 @@ const SideBar = forwardRef<HTMLDivElement, SideBarProps>(({ setIsSideBarOpen, se
   const { pathname } = useLocation();
   const [path, setPath] = useState<string>('');
 
-    useEffect(() => {
-      if (pathname.includes("frame-input")) {
-        setPath("frame-input");
-      } else if (pathname.includes("choose")) {
-        setPath("choose");
-        return;
-      } else if (pathname.includes("image-input")) {
-        setPath("image-input");
-      }
-    }, [pathname]);
-
-    function captureSpecificArea(
-      x: number,
-      y: number,
-      width: number,
-      height: number,
-    ) {
-      html2canvas(document.body, {
-        x,
-        y,
-        width,
-        height,
-        scale: 2,
-      }).then((canvas) => {
-        const link = document.createElement("a");
-        link.href = canvas.toDataURL("image/png", 1);
-        link.download = "specific-area.png";
-        link.click();
-      });
+  useEffect(() => {
+    if (pathname.includes('frame-input')) {
+      setPath('frame-input');
+    } else if (pathname.includes('choose')) {
+      setPath('choose');
+      return;
+    } else if (pathname.includes('image-input')) {
+      setPath('image-input');
     }
+  }, [pathname]);
 
-    const createImageFile = async (): Promise<File> => {
-      const response = await fetch(imgUrl);
-      const blob = await response.blob();
-      const file = new File([blob], "frame.png", { type: blob.type });
-      console.log(file);
+  function captureSpecificArea(x: number, y: number, width: number, height: number) {
+    html2canvas(document.body, {
+      x,
+      y,
+      width,
+      height,
+      scale: 2,
+    }).then((canvas) => {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png', 1);
+      link.download = 'specific-area.png';
+      link.click();
+    });
+  }
 
-      return file;
-    };
+  const createImageFile = async (): Promise<File> => {
+    const response = await fetch(imgUrl);
+    const blob = await response.blob();
+    const file = new File([blob], 'frame.png', { type: blob.type });
+    console.log(file);
+
+    return file;
+  };
 
   const postFrameMutation = useFramePost();
   const formData = new FormData();
@@ -85,40 +73,39 @@ const SideBar = forwardRef<HTMLDivElement, SideBarProps>(({ setIsSideBarOpen, se
     });
   };
 
-    return (
-      <S.Wrapper ref={ref}>
-        <S.Img src={imgUrl} alt={imgUrl} />
-        <S.ButtonContainer path={path}>
+  return (
+    <S.Wrapper ref={ref}>
+      <S.Img src={imgUrl} alt={imgUrl} />
+      <S.ButtonContainer path={path}>
+        <S.Button
+          onClick={() => {
+            if (path === 'frame-input') {
+              navigate('/photo');
+              if (setIsSideBarOpen) {
+                setIsSideBarOpen(false);
+              }
+            }
+          }}
+        >
+          {path === 'frame-input' ? <CameraIcon /> : <EditIcon />}
+        </S.Button>
+        {path !== 'choose' && (
           <S.Button
             onClick={() => {
-              if (path === "frame-input") {
-                navigate("/photo");
-                if (setIsSideBarOpen) {
-                  setIsSideBarOpen(false);
-                }
+              if (path === 'frame-input') {
+                setIsShowModal(true);
+                handleButtonClick();
+              } else if (path !== 'frame-input') {
+                captureSpecificArea(1530, 207.5, 160, 502.588);
               }
             }}
           >
-            {path === "frame-input" ? <CameraIcon /> : <EditIcon />}
+            {path === 'frame-input' ? <ShareIcon /> : <DownLoadIcon />}
           </S.Button>
-          {path !== "choose" && (
-            <S.Button
-              onClick={() => {
-                if (path === "frame-input") {
-                  setIsShowModal(true);
-                  handleButtonClick();
-                } else if (path !== "frame-input") {
-                  captureSpecificArea(1530, 207.5, 160, 502.588);
-                }
-              }}
-            >
-              {path === "frame-input" ? <ShareIcon /> : <DownLoadIcon />}
-            </S.Button>
-          )}
-        </S.ButtonContainer>
-      </S.Wrapper>
-    );
-  },
-);
+        )}
+      </S.ButtonContainer>
+    </S.Wrapper>
+  );
+});
 
 export default SideBar;
